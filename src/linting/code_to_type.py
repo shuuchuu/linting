5a87a1438e2e6835ccb7c310ef5df030379dd6a8
mog <mog@noreply.git.ogion.eu>
2025-09-17 08:57:38 +0200

    Remove intervals from typing exercise
    Signed-off-by: mog <mog@noreply.git.ogion.eu>

--- src/linting/code_to_type.py
+++ src/linting/code_to_type.py
@@ -1,7 +1,5 @@
 """Simple code to test mypy."""
 
-from typing import Any
-
 
 def add(a, b):
     """Compute the sum between two numbers.
@@ -46,47 +44,7 @@ def upper_keys(mapping):
     return result
 
 
-class Interval:
-    """Interval manipulation."""
-
-    def __init__(self, lower_bound: float, upper_bound: float) -> None:
-        """Initialize an interval."""
-        self._lower_bound = lower_bound
-        self._upper_bound = upper_bound
-
-    def upper(self) -> float:
-        """Get the upper bound of the interval.
-
-        Returns:
-            The upper bound of the interval.
-        """
-        return self._upper_bound
-
-    def lower(self) -> float:
-        """Get the lower bound of the interval.
-
-        Returns:
-            The lower bound of the interval.
-        """
-        return self._lower_bound
-
-    def __contains__(self, item: Any) -> bool:
-        """Test if item is in [lower_bound, upper_bound].
-
-        Returns:
-            True if item is in [lower_bound, upper_bound], False otherwise.
-        """
-        try:
-            return self._lower_bound <= item <= self._upper_bound
-        except TypeError:
-            return False
-
-
 if __name__ == "__main__":
     print("add(1, 2) →", add(1, 2))
     print("total([1, 2, 3]) →", total([1, 2, 3]))
     print('upper_keys({"a": 1, "b": 2}) →', upper_keys({"a": 1, "b": 2}))
-    print(
-        'upper_keys({Interval(1.0, 2.0): "abc"})',
-        upper_keys({Interval(1.0, 2.0): "abc"}),
-    )
